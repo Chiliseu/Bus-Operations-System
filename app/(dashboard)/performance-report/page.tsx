4b26f5bc044b2c03d920ec8f26d4b25d9150ec63
RHILIUS <rhianjolius23@gmail.com>
2025-06-11 21:01:47 +0800

    Added initial Dashboard UI to Peroformance Report Tab

--- app/(dashboard)/performance-report/page.tsx
+++ app/(dashboard)/performance-report/page.tsx
@@ -1,12 +1,37 @@
 'use client';
 
 import React from 'react';
+// Style Imports
+import styles from './performance-report.module.css';
+import AreaChart from '@/components/ui/AreaChart';
 
 const PerformanceReportPage: React.FC = () => {
   return (
-    <div className="dashboard-content">
-      <div className="center-box">
-        This is dedicated for PERFORMANCE REPORT
+    <div className={styles.wideCard}>
+      <div className="bg-red-100 text-center">Hindi pa tapos</div>
+      <div className={styles.heading}>Bus Sales</div>
+      {/* Cards */}
+      <div className="grid gap-13 sm:grid-cols-3">
+        {/* Today */}
+        <div className={styles.reportCard}>
+          <p className={styles.cardTitle}>Today</p>
+          <h3 className={styles.amount}>₱9,953.00</h3>
+          <p className={styles.trend}>+20% higher than yesterday</p>
+        </div>
+
+        {/* This Week */}
+        <div className={styles.reportCard}>
+          <p className={styles.cardTitle}>This week</p>
+          <h3 className={styles.amount}>₱56,678.00</h3>
+          <p className={styles.trend}>+10% higher than last week</p>
+        </div>
+
+        {/* This Month */}
+        <div className={styles.reportCard}>
+          <p className={styles.cardTitle}>This month</p>
+          <h3 className={styles.amount}>₱1,653,122.00</h3>
+          <p className={styles.trend}>−15% lower than last month</p>
+        </div>
       </div>
     </div>
   );
