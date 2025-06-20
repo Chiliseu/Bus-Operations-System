6959c86c304ba446b5e63c1709537caf4d5be4f9
Yuan Evangelista <yuanexequielevangelista03@gmail.com>
2025-06-20 17:47:00 +0800

    change the name for BOMS and change the on load now on operational instead of inv

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         />
         
-        <title>Agila Transport System</title>
+        <title>BOMS</title>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
