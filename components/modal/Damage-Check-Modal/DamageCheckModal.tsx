6b51e791491eb8b4d2d345e8039b9d9d22569d9c
Yuan Evangelista <yuanexequielevangelista03@gmail.com>
2025-10-17 21:25:38 +0800

    Bus Rental UI Change V1.2

--- components/modal/Damage-Check-Modal/DamageCheckModal.tsx
+++ components/modal/Damage-Check-Modal/DamageCheckModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
+import Swal from 'sweetalert2';
 import styles from "./damage-check.module.css";
 
 interface DamageCheckModalProps {
